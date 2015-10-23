cee07b10bdfa091f7bfed40545246ddf448c3d5e
MorganSlimak <morganslimak@gmail.com>
2015-10-23 14:06:06 -0400

    modified player controller to communicate with npr api

--- js/main.js
+++ js/main.js
@@ -1,14 +1,16 @@
 var app = angular.module("myApp", []);
 
-app.controller("PlayerController", function($scope) {
-	$scope.playing = false;
-  $scope.audio = document.createElement("audio");
-  $scope.audio.src = "http://a.tumblr.com/tumblr_lmqp4tNmaA1qkdv4bo1.mp3"
-  $scope.play = function() {
-    $scope.audio.play();
-  };
-});
+var apiKey = "MDIwOTYwOTc0MDE0NDU2MTk5MTBhNzJkYg000";
+var nprUrl = 'http://api.npr.org/query?id=61&fields=relatedLink,title,byline,text,audio,image,pullQuote,all&output=JSON';
 
-app.controller("RelatedController", function($scope) {
-	
+
+
+app.controller("PlayerController", function($scope, $http) {
+  $http({
+    url: nprUrl + "&apiKey=" + apiKey + "&callback=JSON_CALLBACK",
+    method: 'jsonp'
+  }).success(function(data, status) {
+    $scope.programs = data.list.story;
+  });
+
 });
